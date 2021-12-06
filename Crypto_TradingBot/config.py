ddec233a0b0f905231b64eb7c3da8c58f1c419d8
Victor Caldeira <victorhenriquebarbosa@gmail.com>
2021-12-06 15:34:37 -0400

    update config.py

--- Crypto_TradingBot/config.py
+++ Crypto_TradingBot/config.py
@@ -1,9 +1,11 @@
+import os
+
 # API key and secret
-api_key = 'Lt3wKiKK2FEFa5ZXyXGzQthMKCOz3C6tCyBq9itLUSCaPTZPxZmbkLIXAjbYlYBV'
-api_secret = 'jkPxdOa4JI0Ub7pXagujWI3UxbwHW0kdneQzoCiktsFnb1FXdU6rl7D2EeryGsU6'
+api_key = os.environ.get('api_key')
+api_secret = os.environ.get('api_secret')
 
 # Strategy config
 asset = 'ETHUSDT'
 window = 11
 trigger = 0.005
-trade_delay = (7200) # minimum delay in seconds between 2 trades
+trade_delay = 7200 # minimum delay in seconds between 2 trades
